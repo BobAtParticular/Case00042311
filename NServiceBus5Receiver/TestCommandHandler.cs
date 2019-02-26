c781ba998bfa3c820638c98bde5089e39714c772
Bob Langley <bob.langley@particular.net>
2019-02-25 16:26:01 -0800

    Implement the handler so it doesn't throw

--- NServiceBus5Receiver/TestCommandHandler.cs
+++ NServiceBus5Receiver/TestCommandHandler.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using Case00042311;
+﻿using Case00042311;
 using NServiceBus;
+using NServiceBus.Logging;
 
 public class TestCommandHandler : IHandleMessages<TestCommand>
 {
     public void Handle(TestCommand message)
     {
-        throw new NotImplementedException();
+        Log.Info("Received a TestCommand message.");
     }
+
+    private static readonly ILog Log = LogManager.GetLogger<TestCommandHandler>();
 }
 
