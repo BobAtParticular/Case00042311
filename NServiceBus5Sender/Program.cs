6d387bc382d6ed6ac53fb23241e0a62d5b9f9762
Bob Langley <bob.langley@particular.net>
2019-02-25 17:02:15 -0800

    Add add a v5 sender

---
+++ NServiceBus5Sender/Program.cs
@@ -0,0 +1,32 @@
+﻿using NServiceBus;
+using Case00042311;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Case00042311.NServiceBus5Sender");
+
+        var transport = busConfiguration.UseTransport<RabbitMQTransport>();
+        transport.ConnectionString("Host=localhost");
+
+        busConfiguration.UseSerialization<BsonSerializer>();
+        //busConfiguration.UseSerialization<JsonSerializer>();
+
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        var bus = Bus.CreateSendOnly(busConfiguration);
+
+        Console.WriteLine("Press Enter key to send a message and exit.");
+        Console.ReadLine();
+
+        bus.Send("Case00042311.NServiceBus5Receiver", new TestCommand
+        {
+            Id = Guid.NewGuid()
+        });
+
+        bus.Dispose();
+    }
+}
